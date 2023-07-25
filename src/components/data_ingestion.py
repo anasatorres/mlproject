6ff061a6a38e5a3f5268b6f1c929b3f24a4e02ca
anapaulasatorres@gmail.com <anapaulasatorres@gmail.com>
2023-07-25 17:31:58 -0300

    data ingestion and transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -2,15 +2,17 @@ import os
 import sys
 from src.exception import CustomException
 from src.logger import logging
-import pandas as pd
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
+import pandas as pd
 from sklearn.model_selection import train_test_split
-from dataclasses import dataclass
 
 class DataIngestionConfig:
-    train_data_path:str = os.path.join('artifacts', 'train.csv')
-    test_data_path:str = os.path.join('artifacts', 'test.csv')
-    raw_data_path:str = os.path.join('artifacts', 'raw.csv')
+    def __init__(self):
+        self.train_data_path = os.path.join('artifacts', 'train.csv')
+        self.test_data_path = os.path.join('artifacts', 'test.csv')
+        self.raw_data_path = os.path.join('artifacts', 'raw.csv')
 
 class DataIngestion:
     def __init__(self):
@@ -40,6 +42,8 @@ class DataIngestion:
         except Exception as e:
             raise CustomException(e, sys)
 
-if __name__=="__main__":
+if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
